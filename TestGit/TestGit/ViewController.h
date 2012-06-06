40585083ebca1c8ea903b95beb88f96df9536fe8
Daniel Heredia <daniel.felipe@naranya.com>
2012-06-06 12:27:13 -0500

    Primer commit desde xcode

---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Daniel Felipe Heredia Saucedo on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestGit/TestGitTests/TestGitTests.h
@@ -0,0 +1,13 @@
+//
+//  TestGitTests.h
+//  TestGitTests
+//
+//  Created by Daniel Felipe Heredia Saucedo on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestGitTests : SenTestCase
+
+@end
